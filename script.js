6780979779b41b429c21a37d5f1e3cce52b7af38
Carol Iantas~ <iantascarol@outlook.com>
2026-05-22 17:37:07 -0600

    Initial commit - Weekly Money Tracker (HTML, CSS)

---
+++ script.js
@@ -0,0 +1,55 @@
+"use strict";
+
+// 2. Select elements
+// get inputs, buttons, and display elements using querySelector
+
+// 3. Variables
+// weeklyBudget = number
+// expenses = array
+
+// 4. saveBudget()
+// get value from input
+// convert to number
+// store in weeklyBudget
+// clear input
+// update summary
+
+// 5. addExpense()
+// get name, amount, category
+// convert amount to number
+// create object with id, name, amount, category
+// push into expenses array
+// clear inputs
+// display expenses
+// update summary
+
+// 6. calculateTotalSpent()
+// loop through expenses
+// sum all amounts
+// return total
+
+// 7. updateSummary()
+// show weekly budget
+// get total spent
+// calculate money left
+// update UI
+
+// 8. displayExpenses()
+// clear list
+// loop through expenses
+// create li for each
+// show data
+// add delete button
+// append to list
+
+// 9. deleteExpense(id)
+// remove item from array
+// update UI
+
+// 10. localStorage
+// save data
+// load data on page start
+
+// 11. Event listeners
+// save button → saveBudget()
+// add button → addExpense()
